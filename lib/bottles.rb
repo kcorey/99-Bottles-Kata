e8338938ec8f2a70928d4829813a9c70eb17dbcd
Ken Corey <kencorey@Kens-MacBook-Pro.local>
2016-07-28 09:14:06 +0100

    Handle multiple cases.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,15 +2,36 @@ class Bottles
 
 	def verse(numberOfBottles)
 
-		result = numberOfBottles.to_s + " bottles of beer on the wall, " \
-				+ numberOfBottles.to_s + " bottles of beer.\n"\
-				+ "Take one down and pass it around, " \
-				+ (numberOfBottles - 1).to_s + " bottle" 
-		if (numberOfBottles - 1 > 1) 
-				result += "s"
-		end
-		result += " of beer on the wall.\n"
+		result = numberOfBottles.to_s + " " + bottlesName(numberOfBottles) + " of beer on the wall, " \
+				+ numberOfBottles.to_s + " " + bottlesName(numberOfBottles) + " of beer.\n"\
+				+ "Take " + pronoun(numberOfBottles) + " down and pass it around, " \
+				+ countInWords(numberOfBottles - 1) + " " + bottlesName(numberOfBottles - 1) \
+				+ " of beer on the wall.\n"
 
 	end
 
+	def pronoun(numberOfBottles)
+		if (numberOfBottles == 1)
+			"it"
+		else
+			"one"
+		end
+	end
+
+	def bottlesName(numberOfBottles)
+		if (numberOfBottles == 1)
+			"bottle"
+		else
+			"bottles"
+		end
+	end
+
+	def countInWords(numberOfBottles)
+		if (numberOfBottles == 0)
+			"no more"
+		else
+			numberOfBottles.to_s
+		end		
+
+	end
 end
