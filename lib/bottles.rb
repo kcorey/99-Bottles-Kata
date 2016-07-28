4ab2732be2caa5ddaea83f834b9f7a5026b7c5b8
Ken Corey <kencorey@Kens-MacBook-Pro.local>
2016-07-28 09:01:32 +0100

    After the 1 bottle left test.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,10 +2,14 @@ class Bottles
 
 	def verse(numberOfBottles)
 
-		numberOfBottles.to_s + " bottles of beer on the wall, " \
+		result = numberOfBottles.to_s + " bottles of beer on the wall, " \
 				+ numberOfBottles.to_s + " bottles of beer.\n"\
 				+ "Take one down and pass it around, " \
-				+ (numberOfBottles - 1).to_s + " bottles of beer on the wall.\n"
+				+ (numberOfBottles - 1).to_s + " bottle" 
+		if (numberOfBottles - 1 > 1) 
+				result += "s"
+		end
+		result += " of beer on the wall.\n"
 
 	end
 
